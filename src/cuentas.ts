c0a40cafbe413b7ec9fb4331206ba07980bb2851
Daniel Fernandez <dfernandezcode@gmail.com>
2023-05-24 20:58:25 +0200

    logic complete. TODO update eslint parameters

--- src/cuentas.ts
+++ src/cuentas.ts
@@ -14,14 +14,25 @@ class Account {
             Account balance is: ${this.balance}
         `)
     }
+
     addFunds(amount: number) {
-        this.balance += amount;
-        console.log(`Added ${amount} to Account ${this.name}`);
-    }
+        if (amount < 0) {
+          console.log("Cannot add a negative amount.");
+        } else {
+          this.balance += amount;
+          console.log(`Added ${amount} to Account ${this.name}`);
+        }
+      }
+      
+      
 
     withdrawFunds(amount: number){
-        this.balance -= amount;
-        console.log(`Removed ${amount} from Account ${this.name}`);
+        if (this.balance <= amount) {
+            console.log("insufficient funds in Account.")
+        }else{
+           this.balance -= amount;
+        console.log(`Removed ${amount} from Account ${this.name}`); 
+        }; 
     } 
 }
 
@@ -38,20 +49,27 @@ Account2.showAccount();
 console.log("-------------------")
 
 // Ingresamos 300€ a la cuenta 1
+console.log("-------------------")
 Account1.addFunds(300);
 console.log("Account Details")
 Account1.showAccount();
-
+console.log("-------------------")
 // Ingresamos un número negativo a la cuenta 2 haciendo que salte un error por consola
+console.log("-------------------")
 Account2.addFunds(-200)
 console.log("Account Details")
 Account2.showAccount();
+console.log("-------------------")
 
 // Retiramos 500€ de la cuenta 1 y la vemos tras la operación
+console.log("-------------------")
 Account1.withdrawFunds(500)
 console.log("Account Details")
 Account1.showAccount();
+console.log("-------------------")
 
 // Retiramos una cantidad mayor a la que hay en la cuenta por lo que se queda a 0.
-cuenta1.retirar(4000);
+console.log("-------------------")
+Account1.withdrawFunds(4000);
 Account1.showAccount();
+console.log("-------------------")
